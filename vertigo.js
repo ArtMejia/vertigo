cbf4cf1961046ba0dc5aa4f53bb871b1b849731f
ArtMejia <arteniomejia.student@careerdevs.com>
2022-06-01 22:11:30 -0400

    first commit

---
+++ vertigo.js
@@ -0,0 +1,37 @@
+let names = ["Samuel", "Hannah" , "Tom", "Grethel", "Mathias", "Tanya", "Art", "Derrel", "Ben", "Genesis", "Lisbeth"]
+
+eachLetter = function() {
+    for (i = 0; i < names.length; i++) {
+        for (j = [0]; j < names[i].length; j++) 
+        console.log(names[i][j]);
+            if (j = [0]) {
+            console.log('\n');
+        }
+    }
+   
+}
+
+eachLetter()
+
+// Create a function that will output he letters of each name vertically
+
+// For example, If I call the function it will output
+ 
+// S
+// a
+// m
+// u
+// e
+// l
+
+
+// H
+// a
+// n
+// n
+// a
+// h
+
+// etc.
+
+// Warning/Hint: Nested Difficulty)
